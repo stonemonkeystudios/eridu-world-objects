dd179e78410c6ec0297ead86495509a4e1380449
Ryan <ryan.nohr@gmail.com>
2022-09-28 16:06:16 -0500

    Missed a semicolon

--- eridu-world-object-host/Program.cs
+++ eridu-world-object-host/Program.cs
@@ -19,8 +19,8 @@ namespace Eridu.WorldObjects {
                     webBuilder
                         .UseKestrel(options => {
                             // WORKAROUND: Accept HTTP/2 only to allow insecure HTTP/2 connections during development.
-                            options.ListenAnyIP(443, listenOptions => { listenOptions.Protocols = HttpProtocols.Http2; }); 
-                            options.AllowAlternateSchemes = true
+                            options.ListenAnyIP(443, listenOptions => { listenOptions.Protocols = HttpProtocols.Http2; });
+                            options.AllowAlternateSchemes = true;
                         })
                         .UseStartup<Startup>();
                 });
